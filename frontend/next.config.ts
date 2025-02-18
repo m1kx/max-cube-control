bc8cb7e1f18ae61d88d65dbd08aa122307bc5bc8
Mika Rottländer <mika.rottlaender@check24.de>
2025-02-18 23:28:10 +0100

    silence sass

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: false,
-  output: "standalone"
+  output: "standalone",
+  sassOptions: {
+    silenceDeprecations: ["legacy-js-api"],
+  },
 };
 
 export default nextConfig;
